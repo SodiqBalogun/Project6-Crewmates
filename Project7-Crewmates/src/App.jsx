9876e5b35daadc88b0b466083b62456243e36f16
SodiqBalogun <sbalogun157@gmail.com>
2024-11-01 15:57:00 -0500

    Making App a browser router for easy linking to pages.

--- Project7-Crewmates/src/App.jsx
+++ Project7-Crewmates/src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react';
 import './App.css';
 import RunThis from './addData.jsx';
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <h1> This is App </h1>
-      <RunThis />
+      <BrowserRouter>
+        <h1> This is App </h1>
+        <RunThis />
+        <div className="sideNav">
+          <div className="links">
+            <h3 className="home"> Home </h3>
+            <h3 className="create"> Create a new Avatar </h3>
+            <h3 className="gallery"> Avatar Gallery </h3>
+          </div>
+        </div>
+        <Routes>
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
