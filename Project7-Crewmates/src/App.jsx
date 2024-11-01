e9ad49e012f0bfba19f703171ff346c5db2bc7f2
SodiqBalogun <sbalogun157@gmail.com>
2024-11-01 16:24:20 -0500

    Working on a side nav.

--- Project7-Crewmates/src/App.jsx
+++ Project7-Crewmates/src/App.jsx
@@ -7,14 +7,19 @@ const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
-        <h1> This is App </h1>
-        <RunThis />
         <div className="sideNav">
           <div className="links">
             <h3 className="home"> Home </h3>
             <h3 className="create"> Create a new Avatar </h3>
             <h3 className="gallery"> Avatar Gallery </h3>
           </div>
+        </div>
+
+        {/* This is to be moved to a separate file/page */}
+        <div className="rest">
+          <h1> Welcome To The Avatar Creator! </h1>
+          <h3> Here, you can create an assortment of different of your liking. </h3>
+          <RunThis />
         </div>
         <Routes>
           
